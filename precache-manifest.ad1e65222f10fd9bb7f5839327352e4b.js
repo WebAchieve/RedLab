4b8c7348d6de1f9018ff0de694ebf4c966f8009c
FLEX <WebAchieve@github.com>
2021-06-30 16:13:50 +0300

    init

---
+++ precache-manifest.ad1e65222f10fd9bb7f5839327352e4b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b64f66b2b89c9f8816",
+    "url": "/RedLab/css/app.89081271.css"
+  },
+  {
+    "revision": "49fb2f7206f3318e48405bbf3d19abc3",
+    "url": "/RedLab/data.json"
+  },
+  {
+    "revision": "c495286fdc3bd004dce13cf5aba929c7",
+    "url": "/RedLab/img/cat.c495286f.svg"
+  },
+  {
+    "revision": "85663da27db94bfed856b3746d83329d",
+    "url": "/RedLab/img/dog.85663da2.svg"
+  },
+  {
+    "revision": "ec0401f8a7d9c8f94105e71869f58a63",
+    "url": "/RedLab/img/fox.ec0401f8.svg"
+  },
+  {
+    "revision": "5cced49d14a02f93db5e1cb90bbea6d1",
+    "url": "/RedLab/img/koala.5cced49d.svg"
+  },
+  {
+    "revision": "3c6574b3d824f91dc49e4c2e94a95740",
+    "url": "/RedLab/img/lion.3c6574b3.svg"
+  },
+  {
+    "revision": "1d9a953741616937b0bb583f57f0a941",
+    "url": "/RedLab/img/owl.1d9a9537.svg"
+  },
+  {
+    "revision": "8e2badb79083647b76740e0f3504915a",
+    "url": "/RedLab/img/penguin.8e2badb7.svg"
+  },
+  {
+    "revision": "296123ee21f7e15e782f0f100100f51e",
+    "url": "/RedLab/img/pig.296123ee.svg"
+  },
+  {
+    "revision": "7c68f01420eaf9261115bfb76591d3a1",
+    "url": "/RedLab/img/raccoon.7c68f014.svg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/RedLab/img/react.11a8f47e.svg"
+  },
+  {
+    "revision": "2e62bc7b325fee3c5ca65a1d0e1c8c2a",
+    "url": "/RedLab/img/sheep.2e62bc7b.svg"
+  },
+  {
+    "revision": "357ae9fb89ab96855fb31f087e80d6b4",
+    "url": "/RedLab/index.html"
+  },
+  {
+    "revision": "68b64f66b2b89c9f8816",
+    "url": "/RedLab/js/app.324d67d2.js"
+  },
+  {
+    "revision": "98d9712fd5421ee14b7a",
+    "url": "/RedLab/js/chunk-vendors.980db403.js"
+  },
+  {
+    "revision": "bec5899829f158ff0d45532c943f6679",
+    "url": "/RedLab/manifest.json"
+  },
+  {
+    "revision": "bf80c094626ee44af80267dfde4ff420",
+    "url": "/RedLab/media/boy.bf80c094.mp4"
+  },
+  {
+    "revision": "93fec09c9929ab21a395f2fd81b8b041",
+    "url": "/RedLab/media/shoe.93fec09c.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/RedLab/robots.txt"
+  }
+]);
